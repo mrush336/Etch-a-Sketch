169ccb986c27dcbbeb0d6f3393fb4a893ac198d9
Matthew Rushford <49460419+mrush336@users.noreply.github.com>
2019-10-16 20:27:36 -0400

    Production/ Work on the js. Got the basics complete.

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const container = document.querySelector('.container');
-//console.log(container);
+
 function createDiv(num){
     for (i=0; i<num; i++){
         let i = document.createElement('div');
         i.classList.add('box');
-        i.textContent = ('abox');
+        i.textContent = ('a box');
         container.appendChild(i);
     }
 }
-
 createDiv(256);
+
+const box = document.querySelectorAll('.box');
+
+function addEvent(item, event, func){
+    for(i=0;i<item.length;i++){
+        item[i].addEventListener(event, func, false);
+    };
+};
+
+function colorChange(){
+    this.style.backgroundColor = 'black'; 
+}
+
+addEvent(box, 'mouseenter', colorChange);
 
